c389d53630b39e9f269feb7ff532b6f17c2f75c1
rl67 <15954445+rl67@users.noreply.github.com>
2021-05-11 21:53:44 +0200

    tagList and tag OK in GraphQL

--- schema/schema.js
+++ schema/schema.js
@@ -22,29 +22,50 @@ var tagLists = [
 
 var tags = [
     { name: 'toRead', id: '1', tagListId: '1' },
-    { name: 'planing', id: '2', tagListid: '2' },
-    { name: 'video', id: '3', tagListid: '1' },
+    { name: 'planing', id: '2', tagListId: '2' },
+    { name: 'video', id: '3', tagListId: '1' },
     { name: 'algorithm' ,id: '4', tagListId: '4' },
     { name: 'speedsNfeeds', id: '5', tagListId: '3' },
     { name: 'jigs', id: '6', tagListId: '1' },
     { name: 'downTails', id: '7', tagListId: '2' }
 ];
 
-const TagListsType = new GraphQLObjectType( {
+const TagListType = new GraphQLObjectType( {
     name: 'TagList',
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString }
     })
-})
+});
+
+const TagType = new GraphQLObjectType({
+    name: 'Tag',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        tagList: { 
+            type: TagListType, 
+            resolve(parent, args) {
+                return _.find(tagLists, { id: parent.tagListId });
+            }
+        }
+    })
+});
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQuery',
     fields: {
         tagLists: {
-            type: new GraphQLList(TagListsType),
+            type: new GraphQLList(TagListType),
             resolve(parent, args) {
                 return tagLists;
+            }
+        },
+        tag: {
+            type: TagType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return _.find(tags, { id: args.id });
             }
         }
     }
