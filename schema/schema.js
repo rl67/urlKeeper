5a0feab0ed3d7730f60702fe98fc3c15a5deb43c
rl67 <15954445+rl67@users.noreply.github.com>
2021-05-12 07:54:38 +0200

    bookMarks in GraphQL

--- schema/schema.js
+++ schema/schema.js
@@ -30,14 +30,29 @@ var tags = [
     { name: 'downTails', id: '7', tagListId: '2' }
 ];
 
+var bookMarks = [
+    { name: 'DZone', id: '1', tagId: '4' },
+    { name: 'Medium', id: '2', tagId: '1' },
+    { name: 'Rob Cosman', id: '3', tagId: '2'},
+    { name: 'Stumpy Nubs', id: '4', tagId: '1'}
+];
+
+// Tag list with its tags
 const TagListType = new GraphQLObjectType( {
     name: 'TagList',
     fields: () => ({
         id: { type: GraphQLID },
-        name: { type: GraphQLString }
+        name: { type: GraphQLString },
+        tags: {
+            type: new GraphQLList(TagType),
+            resolve(parent, args) {
+                return _.filter(tags, { tagListId: parent.id } )
+            }
+        }
     })
 });
 
+// Tag with its parent tagList
 const TagType = new GraphQLObjectType({
     name: 'Tag',
     fields: () => ({
@@ -48,19 +63,51 @@ const TagType = new GraphQLObjectType({
             resolve(parent, args) {
                 return _.find(tagLists, { id: parent.tagListId });
             }
+        },
+        bookmarks: {
+            type: new GraphQLList(BookMarkType),
+            resolve(parent, args) {
+                return _.filter(bookMarks, { tagId: parent.id })
+            }
         }
     })
 });
 
+// Bookmark
+const BookMarkType = new GraphQLObjectType({
+    name: 'BookMark',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        tags: {
+            type: new GraphQLList(TagType),
+            resolve(parent, args) {
+                // 
+            }
+        }
+    })
+});
+
+ 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQuery',
     fields: {
+        // Return a tagList by id, and all its tags
+        tagList: {
+            type: TagListType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return _.find(tagLists, { id: args.id })
+            }
+        },
+        // Return all tagList objects
         tagLists: {
             type: new GraphQLList(TagListType),
             resolve(parent, args) {
                 return tagLists;
             }
         },
+        // Return a tag by its id
         tag: {
             type: TagType,
             args: { id: { type: GraphQLID } },
